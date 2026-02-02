94d4c2f537f501c0bb02f0eb92bc7885921c5ebb
Espinozaje <jeisson12aaron@gmail.com>
2026-02-02 18:26:30 -0500

    first commit

---
+++ src/main/java/com/example/artifex_capital_backend/ArtifexCapitalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.artifex_capital_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtifexCapitalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtifexCapitalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artifex_capital_backend/ArtifexCapitalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artifex_capital_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifexCapitalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
